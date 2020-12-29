b8976c5015f70946faa22b2bb2a3806d0de4bdc1
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-29 20:53:55 +0100

    Create App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { fetchQuestions, Difficulty, Question } from "./_api/fetchQuestions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const questions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.easy);
+      setQuestions(questions);
+      setLoading(false);
+    })();
+  }, []);
+
+  console.log(questions);
+
+  return <div>{loading ? "Loaging..." : "i have data"}</div>;
+};
 
 export default App;
