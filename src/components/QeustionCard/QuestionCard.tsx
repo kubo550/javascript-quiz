90ec3723b81e601a90c725ffebf47de75ffe5955
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-01-04 21:36:45 +0100

    const handleAnswerClick

--- src/components/QeustionCard/QuestionCard.tsx
+++ src/components/QeustionCard/QuestionCard.tsx
@@ -4,14 +4,22 @@ import { Grid, Button } from "@material-ui/core";
 
 interface QuestionCardProps {
   question: QuestionType;
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const QuestionCard: FC<QuestionCardProps> = ({ question }) => {
+const QuestionCard: FC<QuestionCardProps> = ({ question, handleClick }) => {
   return (
     <Grid container spacing={3}>
+      <img src={question.image} alt='Question Image' />
       {question.answers.map((question, i) => (
         <Grid key={i} item xs={12} sm={6}>
-          <Button color='primary' variant='contained' fullWidth value={question.answer}>
+          <Button
+            color='primary'
+            variant='contained'
+            fullWidth
+            value={question.answer}
+            onClick={e => handleClick(e)}
+          >
             {question.answer}
           </Button>
         </Grid>
