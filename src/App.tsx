482078581a32d841d2769be241ae8be6360b373a
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-01-04 21:36:39 +0100

    const handleAnswerClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Container, Grid, Paper, Typography, Button } from "@material-ui/core";
-import { fetchQuestions, QuestionType } from "./_api/fetchQuestions";
+import { Answer, fetchQuestions, QuestionType } from "./_api/fetchQuestions";
 import QuestionCard from "./components/QeustionCard/QuestionCard";
 
 const TOTAL_QUESTIONS = 3;
 
 const App = () => {
+  const [playable, setPlayable] = useState(true);
   const [loading, setLoading] = useState(true);
   const [questions, setQuestions] = useState<QuestionType[]>([]);
-  const [answers, setAnswers] = useState<QuestionType[]>([]);
+  const [answers, setAnswers] = useState<Answer[]>([]);
   const [number, setNumber] = useState(0);
   const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -20,10 +22,28 @@ const App = () => {
     })();
   }, []);
 
+  const callback = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (playable) {
+      const answerText = e.currentTarget.value;
+      const answer = questions[number].answers.filter(q => q.answer === answerText)[0];
+      setAnswers(prev => [...prev, answer]);
+      const score = answer.correct ? 1 : 0;
+      setScore(prev => prev + score);
+    }
+  };
+
   const nextQuestion = () => {
     if (number < questions.length - 1) {
       setNumber(prev => ++prev);
     }
+  };
+
+  const newGame = () => {
+    setAnswers([]);
+    setPlayable(true);
+    setNumber(0);
+    setScore(0);
+    setShowAnswers(false);
   };
 
   if (loading) return <Container> Loading </Container>;
@@ -40,10 +60,7 @@ const App = () => {
             justify='center'
             alignItems='center'
           >
-            <div>
-              <img src={questions[number].image} alt='Quiz Question' />
-            </div>
-            <QuestionCard question={questions[number]} />
+            <QuestionCard question={questions[number]} handleClick={callback} />
           </Grid>
           <Button variant='outlined' color='secondary' onClick={nextQuestion}>
             next question
