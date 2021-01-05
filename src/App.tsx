baae1526cc3125c60c028271a207b1bd83f76c88
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-01-05 19:41:41 +0100

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid, Paper, Typography, Button } from "@material-ui/core";
+import { Container, Grid, Paper, Typography, Button, Box } from "@material-ui/core";
 import { Answer, fetchQuestions, QuestionType } from "./_api/fetchQuestions";
 import QuestionCard from "./components/QeustionCard/QuestionCard";
+import NextButton from "./components/NextButton/NextButton";
 
-const TOTAL_QUESTIONS = 3;
+const TOTAL_QUESTIONS = 15;
 
 const App = () => {
   const [playable, setPlayable] = useState(true);
@@ -27,7 +28,7 @@ const App = () => {
       const answerText = e.currentTarget.value;
       const answer = questions[number].answers.filter(q => q.answer === answerText)[0];
       setAnswers(prev => [...prev, answer]);
-      const score = answer.correct ? 1 : 0;
+      const score = +answer.correct;
       setScore(prev => prev + score);
     }
   };
@@ -46,25 +47,68 @@ const App = () => {
     setShowAnswers(false);
   };
 
-  if (loading) return <Container> Loading </Container>;
+  if (loading) return <Container> Loading... </Container>;
 
   return (
     <Container>
       <Grid container spacing={3} justify='center' alignItems='center'>
         <Paper>
-          <Typography variant='h4'> {questions[number].question} </Typography>
+          <Typography variant='h5' style={{ marginTop: "20px" }}>
+            {number + 1} / {TOTAL_QUESTIONS}
+          </Typography>
+
+          <Typography variant='h5' style={{ marginTop: "20px" }}>
+            {questions[number].question}
+          </Typography>
           <Grid
             container
             spacing={3}
             direction='column'
             justify='center'
             alignItems='center'
+            style={{ marginTop: "20px" }}
           >
-            <QuestionCard question={questions[number]} handleClick={callback} />
+            <QuestionCard
+              question={questions[number]}
+              answered={answers.length === number + 1}
+              handleClick={callback}
+            />
           </Grid>
-          <Button variant='outlined' color='secondary' onClick={nextQuestion}>
-            next question
-          </Button>
+          {answers.length === number + 1 && answers.length !== TOTAL_QUESTIONS && (
+            <NextButton nextQuestion={nextQuestion} />
+          )}
+          {answers.length === TOTAL_QUESTIONS && (
+            <>
+              <Typography variant='subtitle1'>
+                Your Score {score} / {TOTAL_QUESTIONS}
+              </Typography>
+              <Button variant='contained' color='primary' onClick={newGame}>
+                Restart Game
+              </Button>
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={() => setShowAnswers(true)}
+              >
+                Show Answers
+              </Button>
+            </>
+          )}
+
+          {showAnswers &&
+            answers.map((answer, i) => (
+              <div key={i}>
+                <h4> Question #{i + 1} </h4>
+
+                <img src={questions[i].image} alt='Question' />
+                <h5 className={answer.correct ? "correct" : "incorrect"}>
+                  Your Answer: {answer.answer}
+                </h5>
+                <h5>
+                  Correct Answer: {questions[i].answers.filter(a => a.correct)[0].answer}
+                </h5>
+              </div>
+            ))}
         </Paper>
       </Grid>
     </Container>
